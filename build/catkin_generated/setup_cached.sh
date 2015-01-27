d842d23bd192b4c3b3c604c7285f046a09d7d740
Dulluhan <yuanv@alumni.ubc.ca>
2015-01-27 13:18:41 -0800

    built catkin_workspace

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+export CATKIN_TEST_RESULTS_DIR="/home/dulluhan/Desktop/ROS_GPS/build/test_results"
+export ROS_TEST_RESULTS_DIR="/home/dulluhan/Desktop/ROS_GPS/build/test_results"
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/dulluhan/Desktop/ROS_GPS/devel:$CMAKE_PREFIX_PATH"
+export CPATH="/home/dulluhan/Desktop/ROS_GPS/devel/include:$CPATH"
+export LD_LIBRARY_PATH="/home/dulluhan/Desktop/ROS_GPS/devel/lib:/home/dulluhan/Desktop/ROS_GPS/devel/lib/i386-linux-gnu:$LD_LIBRARY_PATH"
+export PATH="/home/dulluhan/Desktop/ROS_GPS/devel/bin:$PATH"
+export PKG_CONFIG_PATH="/home/dulluhan/Desktop/ROS_GPS/devel/lib/pkgconfig:/home/dulluhan/Desktop/ROS_GPS/devel/lib/i386-linux-gnu/pkgconfig:$PKG_CONFIG_PATH"
+export PWD="/home/dulluhan/Desktop/ROS_GPS/build"
+export PYTHONPATH="/home/dulluhan/Desktop/ROS_GPS/devel/lib/python2.7/dist-packages:$PYTHONPATH"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/dulluhan/Desktop/ROS_GPS/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/dulluhan/Desktop/ROS_GPS/src:$ROS_PACKAGE_PATH"
